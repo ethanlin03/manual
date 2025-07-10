afb64a8c52f849a75412c5247c780e9f2dff662f
Ethan Lin <midgetlin2003@gmail.com>
2025-07-10 02:12:34 -0700

    Templates for track and profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Track() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-black text-xl">Profile Page</Text>
-    </View>
-  );
+export default function Profile() {
+	return (
+		<SafeAreaView className="flex-1 max-h-screen items-center justify-start bg-white pb-20">
+			<View className="items-center justify-center px-6 pb-20 w-full">
+				<View className="flex flex-col items-center mt-10 h-[40vh]">
+					<View className="border-2 border-gray-500 rounded-full p-8 mb-4">
+						<Ionicons name="person" size={60} color="#6b7280" />
+					</View>
+					<Text className="font-bold text-2xl text-[#6b7280]">Ethan Lin</Text>
+				</View>
+
+				<View className="flex flex-col w-[90vw] space-y-20 h-auto">
+					<Text className="font-bold text-xl text-black">Service coming up</Text>
+					<Text className="font-bold text-xl text-black">Recently serviced</Text>
+					<Text className="font-bold text-xl text-black">Most used</Text>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
 }
--- app/(tabs)/track.tsx
+++ app/(tabs)/track.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import { ScrollView, Text, TextInput, TouchableOpacity, View, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react';
 
 export default function Track() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-black text-xl">Track Page</Text>
-    </View>
-  );
+	const [searchedCar, setSearchedCar] = useState("");
+	const openFilters = () => {
+		console.log("Filters opened");
+	}
+
+	return (
+		<SafeAreaView className="flex-1 bg-white">
+			<Text className="font-bold text-2xl ml-10">Track Page</Text>
+			<View className="flex flex-row items-center justify-between  w-[90vw] self-center">
+				<View className="flex-grow flex-row py-2 px-4 rounded-lg bg-gray-200 mr-4">
+					<Ionicons name="search" size={14} color="#888"/>
+					<TextInput
+						placeholder="Search for car..."
+						placeholderTextColor="#888"
+						value={searchedCar}
+						onChangeText={setSearchedCar}
+						className="ml-2"
+					/>
+				</View>
+				<TouchableOpacity onPress={openFilters}>
+					<Ionicons name="filter" size={14} color="#888"/>
+				</TouchableOpacity>
+			</View>
+			<ScrollView className="flex-1 mx-auto">
+				
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
