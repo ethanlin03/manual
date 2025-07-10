4771ed178ddc4f5687896b64a0d64347527a238a
Ethan Lin <midgetlin2003@gmail.com>
2025-07-10 16:15:26 -0700

    Created base template for adding a new car

--- app/car/addcar.tsx
+++ app/car/addcar.tsx
@@ -1,21 +1,97 @@
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
 
 const AddCar = () => {
     const router = useRouter();
+    const [image, setImage] = useState<string | null>(null);
+    const [carName, setCarName] = useState("");
+    const [carYear, setCarYear] = useState("");
+    const [carMake, setCarMake] = useState("");
+    const [carModel, setCarModel] = useState("");
+    const [mileage, setMileage] = useState("");
+
     const handleBack = () => {
 		router.push('/')
 	};
+    const handleNextPage = () => {
+        console.log("Next page");
+    }
+    const handleIamgeSelection = () => {
+        console.log("Image selected");
+    }
 
     return (
-        <SafeAreaView className="flex-1 bg-white">
-			<TouchableOpacity onPress={handleBack} className="p-4">
-				<Ionicons name="arrow-back" size={20}/>
-			</TouchableOpacity>
-			<View className="flex-1 justify-center items-center">
-				<Text className="text-2xl font-bold">Add car</Text>
-			</View>
+        <SafeAreaView className="flex-1 bg-white p-4">
+			<View className="flex p-4">
+                <TouchableOpacity onPress={handleBack}>
+                    <Ionicons name="arrow-back" size={20}/>
+                </TouchableOpacity>
+                <View className="flex justify-start items-center mb-6">
+                    <Text className="text-2xl font-bold mb-2">Add a new car</Text>
+                    {/* Image selection section */}
+                    <TouchableOpacity onPress={handleIamgeSelection} className="flex flex-col items-center justify-center w-[90vw] aspect-[16/9] bg-blue-200 rounded-xl border-2 border-blue-400">
+                        <Ionicons name="image-outline" size={30}/>
+                        <Text className="mt-2 font-semibold">Select an image</Text>
+                    </TouchableOpacity>
+                </View>
+                <View className="flex flex-col p-2 min-w-full">
+                    <Text className="text-sm italic mb-1">Car name:</Text>
+                    <View className="flex-grow flex-row py-2 px-2 rounded-lg bg-gray-200 mb-6">
+                        <TextInput
+                            placeholder="Enter car name..."
+                            placeholderTextColor="#888"
+                            value={carName}
+                            onChangeText={setCarName}
+                            className="ml-2"
+                        />
+                    </View>
+                    <Text className="text-sm italic mb-1">Car year:</Text>
+                    <View className="flex-grow flex-row py-2 px-2 rounded-lg bg-gray-200 mb-6">
+                        <TextInput
+                            placeholder="Enter car year..."
+                            placeholderTextColor="#888"
+                            value={carYear}
+                            onChangeText={setCarYear}
+                            className="ml-2"
+                        />
+                    </View>
+                    <Text className="text-sm italic mb-1">Car make:</Text>
+                    <View className="flex-grow flex-row py-2 px-2 rounded-lg bg-gray-200 mb-6">
+                        <TextInput
+                            placeholder="Enter car make..."
+                            placeholderTextColor="#888"
+                            value={carMake}
+                            onChangeText={setCarMake}
+                            className="ml-2"
+                        />
+                    </View>
+                    <Text className="text-sm italic mb-1">Car model:</Text>
+                    <View className="flex-grow flex-row py-2 px-2 rounded-lg bg-gray-200 mb-6">
+                        <TextInput
+                            placeholder="Enter car model..."
+                            placeholderTextColor="#888"
+                            value={carModel}
+                            onChangeText={setCarModel}
+                            className="ml-2"
+                        />
+                    </View>
+                    <Text className="text-sm italic mb-1">Car mileage:</Text>
+                    <View className="flex-grow flex-row py-2 px-2 rounded-lg bg-gray-200 mb-20">
+                        <TextInput
+                            placeholder="Enter car mileage..."
+                            placeholderTextColor="#888"
+                            value={mileage}
+                            onChangeText={setMileage}
+                            className="ml-2"
+                        />
+                    </View>
+                    <TouchableOpacity onPress={handleNextPage} className="flex flex-row items-end justify-end">
+                        <Ionicons name="arrow-forward" size={16}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
 		</SafeAreaView>
     );
 };
