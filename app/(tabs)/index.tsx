1346498fa968e4800ccfa23a467c1a1d2acb600e
Ethan Lin <midgetlin2003@gmail.com>
2025-07-09 22:46:02 -0700

    Added filler car cards and formatted home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native';
+import { ScrollView, Text, View, SafeAreaView } from 'react-native';
 import CarCard from '@/components/CarCard';
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-black text-2xl">Home Page</Text>
-      <CarCard/>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+		<ScrollView className="flex-1 mx-auto">
+			<Text className="font-bold text-2xl">Home Page</Text>
+			<View className="space-y-4 pb-16">
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+				<CarCard />
+			</View>
+		</ScrollView>
+    </SafeAreaView>
   );
 }
-
--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,15 +1,20 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
-import Car from '@/assets/images/stock_car.png';
+import { useRouter } from 'expo-router';
+import Car from '@/assets/images/car.png';
 
 const CarCard: React.FC = () => {
-    
+    const router = useRouter();
+
+    const handleCarSelect = () => {
+        router.push('/history');
+    };
     return (
-        <TouchableOpacity className="flex flex-row items-center shadow-gray-500 shadow-sm max-w-[95vw] w-full bg-white border-b-1 border-gray-500 p-2">
-            <Image source={Car} className="max-w-[30vw] h-auto aspect-[16/9]"/>
-            <View className="flex flex-col items-start space-y-2">
-                <Text>Car Name</Text>
-                <Text>Year, Make, Model</Text>
-                <Text>Current Mileage</Text>
+        <TouchableOpacity onPress={handleCarSelect} className="flex flex-row items-center shadow-gray-500 shadow-sm max-w-[95vw] w-full bg-white border-b-1 border-gray-500 p-2">
+            <Image source={Car} className="max-w-[30vw] h-auto aspect-[4/3] mr-2"/>
+            <View className="flex flex-col items-start justify-start space-y-2 mb-auto">
+                <Text className="text-lg font-semibold mb-1">16_ct2</Text>
+                <Text className="text-sm italic text-gray-500">2016 Honda Accord</Text>
+                <Text className="text-sm">70,000 mi</Text>
             </View>
             <View className="flex items-center justify-center ml-auto mb-auto w-6 h-6 rounded-full bg-red-400">
                 <Text className="font-semibold text-sm">
