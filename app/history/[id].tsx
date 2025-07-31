aa2ec7cec1ede2d63db758017d01d2cb8fe87c93
Ethan Lin <midgetlin2003@gmail.com>
2025-07-30 23:59:19 -0700

    Template for car history page

--- app/history/[id].tsx
+++ app/history/[id].tsx
@@ -33,9 +33,21 @@ export default function HistoryPage() {
 			</TouchableOpacity>
 			<CarDropDownMenu/>
 
-			<View className="flex-1 justify-center items-center">
+			<View className="flex items-center border-b-1 mb-10">
+				<Image source={CarImg} className="h-[20vh] aspect-[4/3] rounded-lg"/>
 				<Text className="text-2xl font-bold">Car History for ID #{id}</Text>
 			</View>
+			<View className="p-4">
+				<Text className="font-bold text-xl text-black">
+					Service History
+				</Text>
+			</View>
+			{/*<View className="flex-1 relative p-2">
+				<View className="absolute left-1/4 top-0 bottom-0 w-px bg-black z-0" style={{ borderStyle: 'dashed' }}/>
+				<View className="absolute left-1/4">
+					<Ionicons name='car' size={24}/>
+				</View>
+			</View>*/}
 		</SafeAreaView>
 	);
 }
