6f23ffab7cbba80aebcc2d7656cb03202f25ab6a
Ethan Lin <midgetlin2003@gmail.com>
2025-08-20 19:53:45 -0700

    Alert modal for unsaved changes

--- components/MaintenanceModal.tsx
+++ components/MaintenanceModal.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { View, Text, TextInput, Pressable } from "react-native";
 import { LabeledField } from "./LabeledField";
 import { Ionicons } from "@expo/vector-icons";
@@ -19,24 +19,53 @@ export const MaintenanceModal = ({maintenance, setMaintenanceModal, months, setM
 
     const handleSubmit = () => {
         setMaintenanceModal(false);
+    };
+
+    const handleCancel = () => {
+        console.log("cancel")
+        setAlertModal(false);
     }
 
-    // const handleClose = () => {
-    //     if(tempMiles !== 0 || tempMonths !== 0) {
-    //         alert("You have unsaved changes!");
-    //         return;
-    //     }
-    //     setMaintenanceModal(false);
-    // };
+    const handleClose = () => {
+        if(tempMiles !== 0 || tempMonths !== 0) {
+            setAlertModal(true);
+            return;
+        }
+        setMaintenanceModal(false);
+    };
+
+    useEffect(() => {
+        console.log(tempMonths);
+    }, [tempMonths])
 
     return (
-        <Pressable onPress={() => setMaintenanceModal(false)} className="absolute justify-center items-center inset-0 bg-black/50 z-10">
+        <Pressable onPress={handleClose} className="absolute justify-center items-center inset-0 bg-black/50 z-10">
+            {alertModal && 
+                <Pressable onPress={() => {}} className="absolute justify-center items-center inset-0 bg-black/80 z-10">
+                    <View className="flex flex-col justify-center w-[70%] bg-white rounded-lg h-auto p-4 gap-2">
+                        <Text className="text-gray-700 font-semibold text-2xl self-center">
+                            Confirm
+                        </Text>
+                        <Text className="text-sm">
+                            Are you sure you want to leave? You have unsaved changes.
+                        </Text>
+                        <View className="flex flex-row self-end gap-2 mt-2">
+                            <Pressable onPress={handleSubmit} className="w-auto bg-blue-200 p-2 rounded-lg">
+                                <Text className="text-sm font-semibold">Confirm</Text>
+                            </Pressable>
+                            <Pressable onPress={handleCancel} className="w-auto bg-white border p-2 rounded-lg">
+                                <Text className="text-sm font-semibold">Cancel</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </Pressable>
+            }
             <Pressable onPress={() => {}} className="w-[60%] bg-white rounded-lg h-auto p-4 gap-2">
                 <View className="relative items-center mb-2">
                     <Ionicons 
                         name="close-circle-outline" 
                         size={16} 
-                        onPress={() => setMaintenanceModal(false)} 
+                        onPress={handleClose} 
                         className="absolute left-0" 
                     />
                     <Text className="text-gray-700 font-semibold text-lg">
